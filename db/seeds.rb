404b27212ad92632cf717eaf605f666aa463f7c2
ss-siranjeevi <siranjeevi@softsuave.com>
2022-01-17 17:22:44 +0530

    refactor the seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,38 @@
-3.times do |MUG|
-	Item.create!(
-        code: "MUG",
-		name: "Reedsy Mug",
-        price: 6,
-        offer_id: 1
-	)
-end
-
-3.times do |TSHIRT|
-	Item.create!(
-		code: "TSHIRT",
-		name: "Reedsy T-shirt",
-        price: 15,
-        offer_id: 2
-	)
-end
-
-3.times do |HOODIE|
-	Item.create!(
-		code: "HOODIE",
-		name: "Reedsy Hoodie",
-        price: 20,
-        offer_id: 3
-	)
-end
-
 Offer.create!(
-    category: "BUY2GET1"
+  category: 'BUY2GET1'
 )
 
 Offer.create!(
-    category: "30%"
+  category: '30%'
 )
 
 Offer.create!(
-    category: "NOOFFER"
+  category: 'NOOFFER'
 )
+
+3.times do |_i|
+  Item.create!(
+    code: 'MUG',
+    name: 'Reedsy Mug',
+    price: 6,
+    offer_id: 1
+  )
+end
+
+3.times do |_i|
+  Item.create!(
+    code: 'TSHIRT',
+    name: 'Reedsy T-shirt',
+    price: 15,
+    offer_id: 2
+  )
+end
+
+3.times do |_i|
+  Item.create!(
+    code: 'HOODIE',
+    name: 'Reedsy Hoodie',
+    price: 20,
+    offer_id: 3
+  )
+end
