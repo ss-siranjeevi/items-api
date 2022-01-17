1da6794ea6e59cd288240814751955bd70684e99
ss-siranjeevi <siranjeevi@softsuave.com>
2022-01-17 23:10:24 +0530

    updated spec file

--- spec/controllers/items_controller.spec.rb
+++ spec/controllers/items_controller.spec.rb
@@ -28,9 +28,7 @@ describe 'ItemsController', type: :request do
     let(:params) { { item: { code: 'TSHIRT', name: 'Roadster', price: 9 } } }
     it 'updated successfully' do
       put "/items/#{new_item.id}", params: params
-      item = JSON.parse(response.body)['item']
-      expect(response.status).to eq 200
-      expect(item["price"].to_i).to eq 9
+      expect(response).to redirect_to(items_path)
     end
   end
 
@@ -43,21 +41,21 @@ describe 'ItemsController', type: :request do
     end
   end
 
-  # describe 'place_order' do
-  #   let(:new_item1) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15 }) }
-  #   let(:new_item2) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15 }) }
-  #   let(:new_item3) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15 }) }
-  #   let(:new_item4) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15 }) }
-  #   let(:new_item5) { Item.create!({ code: 'MUG', name: 'Mast Harb', price: 6 }) }
-  #   let(:new_item6) { Item.create!({ code: 'MUG', name: 'Mast Harb', price: 6 }) }
-  #   let(:new_item7) { Item.create!({ code: 'HOODIE', name: 'Super Dry', price: 20 }) }
-  #   let(:params) { { items: [new_item1, new_item2, new_item3, new_item4, new_item5,new_item6, new_item7 ]  } }
-  #   it 'successfully place the order' do
-  #     get 'items/place_order', params: params
-  #     order = JSON.parse(response.body)['order']
-  #     expect(response.status).to_eq 200
-  #     expect(order['price']).to 74
-  #   end
-  # end
+  describe 'place_order_with_order' do
+    let(:new_item1) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15, offer_id: 2 }) }
+    let(:new_item2) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15, offer_id: 2 }) }
+    let(:new_item3) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15, offer_id: 2 }) }
+    let(:new_item4) { Item.create!({ code: 'TSHIRT', name: 'Roadster', price: 15, offer_id: 2 }) }
+    let(:new_item5) { Item.create!({ code: 'MUG', name: 'Mast Harb', price: 6, offer_id: 1 }) }
+    let(:new_item6) { Item.create!({ code: 'MUG', name: 'Mast Harb', price: 6, offer_id: 1 }) }
+    let(:new_item7) { Item.create!({ code: 'HOODIE', name: 'Super Dry', price: 20, offer_id: 3 }) }
+    let(:params) { { items: [new_item1, new_item2, new_item3, new_item4, new_item5,new_item6, new_item7 ]  } }
+    it 'successfully place the order' do
+      get 'items/place_order_with_order', params: params
+      order = JSON.parse(response.body)['order']
+      expect(response.status).to_eq 200
+      expect(order['price']).to 74
+    end
+  end
 
 end
