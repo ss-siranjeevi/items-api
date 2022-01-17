dcbc341db77f128634c6869e47c4c9819e0abfe8
ss-siranjeevi <siranjeevi@softsuave.com>
2022-01-17 17:18:24 +0530

    add place_order api

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,38 +1,39 @@
 class ItemsController < ApplicationController
-        def index
-            @items = Item.all
-            render json: { status: 'Success', message: 'Presented items', items: @items }, status: :ok
-        end
+  def index
+    @items = Item.all
+  end
 
-        def show
-            item = Item.find(params[:id])
-            render json: { status: 'Success', message: 'Item found', item: item }, status: 302
-        end
+  def show
+    item = Item.find(params[:id])
+    render json: { status: 'Success', message: 'Item found', item: item }
+  end
 
-        def create
-            item = Item.new(item_params)
-            item.save!
-            render json: { status: 'Success', message: 'Item created', item: item }, status: 201
-        end
+  def create
+    item = Item.new(item_params)
+    item.save!
+    render json: { status: 'Success', message: 'Item created', item: item }
+  end
 
-        def update
-            item = Item.find(params[:id])
-            item.update!(item_params)
-            render json: { status: 'Success', message: 'Item updated', item: item }, status: :ok
-        end
+  def update
+    item = Item.find(params[:id])
+    item.update!(item_params)
+    render json: { status: 'Success', message: 'Item updated', item: item }
+  end
 
-        def destroy
-            item = Item.find(params[:id])
-            item.destroy!
-            render json: { status: 'Success', message: 'Item deleted' }, status: :ok
-        end
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy!
+    render json: { status: 'Success', message: 'Item deleted' }
+  end
 
+  def place_order
+   total_price = PlaceOrderService.new(params[:item]).call
+   render json: { status: 'Success', message: 'Item deleted', total_price: total_price }
+  end
 
-        private
+  private
 
-        def item_params
-            params.require(:item).permit(:code, :name, :price)
-        end
-
+  def item_params
+    params.require(:item).permit(:code, :name, :price)
+  end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :items
+  post 'items/place_order', to: 'items#place_order'
 end
