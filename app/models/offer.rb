1317739f63f93c24a0573f2a8520fe4722ce9b3d
ss-siranjeevi <siranjeevi@softsuave.com>
2022-01-17 17:21:42 +0530

    define  method to get price based on category type

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,17 @@
 class Offer < ApplicationRecord
+
+  enum type: ['BUY2GET1', '30%', 'NOOFFER']
+
+  def self.get_price(type, price, no_quantity)
+    case type
+    when 'BUY2GET1'
+      ((no_quantity / 3) * (price * 2)) + ((no_quantity / 3) * price)
+    when '30%'
+      return price - (price * 30) / 100 if no_quantity >= 3
+
+      price
+    else
+      price
+    end
+  end
 end
