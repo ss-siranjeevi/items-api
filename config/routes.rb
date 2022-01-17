bb4488c23f1ee5f0c31a0ea1f398e1d7a3f3ddf2
ss-siranjeevi <siranjeevi@softsuave.com>
2022-01-17 23:10:39 +0530

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :items
   post 'items/place_order', to: 'items#place_order'
+  post 'items/place_order_with_offer', to:'items#place_order_with_offer'
+  post "items/add_to_cart/:id", to: "items#add_to_cart", as:"add_to_cart"
+  delete "items/remove_from_cart/:id", to: "items#remove_from_cart", as:"remove_from_cart"
+  post "items/add_items/:id", to: "items#add_items", as: "add_items"
 end
