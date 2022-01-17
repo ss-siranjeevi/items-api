2ab133150b0581f8d0c284a5915a582f15a19f47
ss-siranjeevi <siranjeevi@softsuave.com>
2022-01-17 23:05:16 +0530

    add session storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    before_action :initialize_session
+    before_action :load_cart
+
+    def initialize_session
+        session[:cart] ||= []
+    end
+
+    def load_cart
+        @cart = Item.find(session[:cart])
+    end
 end
